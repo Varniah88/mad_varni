230e06f4a66b66d7dc5bcd0f8a8466a8b4940eea
varniah88 <jaanuvarni@gmail.com>
2019-09-07 17:57:44 +0530

    chfegfeg

--- app/src/main/java/com/example/app/view_feedback.java
+++ app/src/main/java/com/example/app/view_feedback.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class view_feedback extends AppCompatActivity {
-//dilin
+//varniah
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
