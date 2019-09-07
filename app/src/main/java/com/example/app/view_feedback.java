f13b541083d73e81e519e013c6776bbaad078e59
Dilan <vasandarajdilan64@gmail.com>
2019-09-07 17:53:45 +0530

    Update view_feedback.java

--- app/src/main/java/com/example/app/view_feedback.java
+++ app/src/main/java/com/example/app/view_feedback.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class view_feedback extends AppCompatActivity {
-
+//dilin
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_feedback);
+
     }
 }
